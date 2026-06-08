0eb115667929c0ed5bb622223089dd63266ba550
moljac <mcvjetko@holisticware.net>
2026-06-08 19:32:25 +0200

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
-source ./native/build-all.sh osx-arm64
-source ./native/build-all.sh osx-x64
 source ./native/build-all.sh linux-x64
 source ./native/build-all.sh linux-arm64
 source ./native/build-all.sh win-x64
+
+
+source ./native/build-all.sh osx-arm64
+source ./native/build-all.sh osx-x64
+
