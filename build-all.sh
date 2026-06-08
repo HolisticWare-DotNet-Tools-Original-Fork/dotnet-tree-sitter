86759effed7babbc44f98316e00843c9ba8daaaf
moljac <mcvjetko@holisticware.net>
2026-06-08 17:38:51 +0200

    Create build-all.sh

---
+++ build-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+source ./native/build-all.sh osx-arm64
+source ./native/build-all.sh osx-x64
+source ./native/build-all.sh linux-x64
+source ./native/build-all.sh linux-arm64
+source ./native/build-all.sh win-x64
