4f430f11193cb825de846740ad56ba4739d72e78
moljac <mcvjetko@holisticware.net>
2026-06-09 13:16:20 +0200

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+cd ./native/
 
-source ./native/build-all.sh linux-x64
-source ./native/build-all.sh linux-arm64
-source ./native/build-all.sh win-x64
+# naive build
+source ./build-all.sh osx-arm64
+source ./build-all.sh osx-x64
+# xcompile
+source ./build-all.sh linux-x64
+source ./build-all.sh linux-arm64
+source ./build-all.sh win-x64
 
+cd -
 
-source ./native/build-all.sh osx-arm64
-source ./native/build-all.sh osx-x64
+tree native/out/
 
+file ./native/out/linux-arm64/libtree-sitter.so
+file ./native/out/linux-x64/libtree-sitter.so
+file ./native/out/win-x64/libtree-sitter.so
+file ./native/out/osx-arm64/libtree-sitter.so
+file ./native/out/osx-x64/libtree-sitter.so
